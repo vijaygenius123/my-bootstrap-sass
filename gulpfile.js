8eafd7a0079f2d7a69a4154eefc6c422647333f4
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-19 07:34:48 +0000

    Refactor To Maintain

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles(){
-    return src('index.scss')
+    return src('dopi/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 function watchTask(){
-    watch(['index.scss'], buildStyles)
+    watch(['dopi/**/*/.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
